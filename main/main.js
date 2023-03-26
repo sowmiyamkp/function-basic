5e9c1843298f347ad2bcaec04e1318ae89d2575d
sowmiyamkp <sowmiyamkp1305@gmail.com>
2023-03-25 20:41:55 -0700

    add function-basic

---
+++ main/main.js
@@ -0,0 +1,23 @@
+
+						//function
+						
+function add()
+{
+	
+	let a=20,b=15,c;
+	c=a+b;
+	console.log(c);
+}
+add();
+
+
+
+
+function sum()
+   {
+	var x=10,y=12,z;
+	z=x+y;
+	return z;
+	}
+	var result=sum();
+	console.log(result/2);
